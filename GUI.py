c1804a9cc20b781f67f1dec0e75bb1271f4b6153
Tecrato <pinchos792003@gmail.com>
2024-02-28 19:22:07 -0400

    Mejorada las GUI (Return_text)
    Ahora se debe configurar un path para la fuente de los simbolos para poder funcionar.
    
    Añadido el boton de pegar del portapapeles en el return_text.
    
    Añanido texto con el ejemplo de codigo para hacer funcionar la libreria.

--- GUI.py
+++ GUI.py
@@ -1,13 +1,36 @@
-import pygame as pag
+import pygame as pag, pyperclip
+from pathlib import Path
 
 from .text import Create_boton, Create_text, Input_text
 from pygame.math import Vector2
 
+configs = {}
+
 class GUI_admin:
-    def __init__(self,surface) -> None:
-        self.surface = surface
+    """
+    # Ejemplo:
+
+    ### Para los eventos de los inputs:
+    self.GUI_manager.input_update(eventos)
+
+    ### Para los eventos:
+    (Debajo del evento de QUIT)
+    elif self.GUI_manager.active >= 0:
+        if evento.type == KEYDOWN and evento.key == K_ESCAPE:
+            self.GUI_manager.pop()
+        elif evento.type == MOUSEBUTTONDOWN and evento.button == 1:
+            self.GUI_manager.click((mousex,mousey))
+
+    ### Para dibujar:
+    self.GUI_manager.draw(self.ventana,(mousex,mousey))
+    """
+    def __init__(self) -> None:
         self.__list = []
         self.active = -1
+        try:
+            configs['fuente_simbolos']
+        except Exception as err:
+            raise Exception('Debes incluir la siguiente linea de codigo: Utilidades.GUI.configs[\'fuente_simbolos\'] = "tu path hasta la fuente de symbols"')
     def add(self,clase, func= None) -> None:
         self.__list.append({'GUI':clase.copy(), 'func':func})
         self.active = len(self.__list)-1
@@ -23,10 +46,11 @@ class GUI_admin:
             self.active = -1 if not self.__list else 0
             return (self.active,result['result']())
         elif result and result['return'] in ['destroy', 'cancelar']:
-            # if self.__list[self.active]['func']: self.__list[self.active]['func'](False)
             self.pop(self.active)
             self.active = -1 if not self.__list else 0
             return False
+        elif result and result['return'] == 'function':
+            self.active,result['result']()
         return False
     def pop(self,index=-1) -> None:
         if len(self.__list) > 0:
@@ -41,8 +65,6 @@ class Base_win:
     def __init__(self, centro, title) -> None:
         self.rect = pag.Rect(0,0,500,300)
         self.rect.center = centro
-
-        # self.topleft = Vector2(self.rect.topleft) - (Vector2(self.rect.topleft)*(2,2))
 
         self.surface: pag.Surface = pag.Surface((500,300), pag.SRCALPHA)
         pag.draw.rect(self.surface,'white',[0,0,500,300], border_radius=20)
@@ -86,7 +108,6 @@ class Info(Base_win):
     def __init__(self,centro: tuple[int,int],encabezado: str,text: str|list[str]):
         super().__init__(centro,encabezado)
 
-        # Create_text(encabezado,40,None,(250,70),'center', 'black').draw(self.surface)
         Create_text(text,25,None,(30,120),'left', 'black').draw(self.surface)
 
         self.botones.append({
@@ -99,7 +120,6 @@ class Desicion(Base_win):
     def __init__(self,centro: tuple[int,int],encabezado: str,text: str|list[str]):
         super().__init__(centro,encabezado)
 
-        # Create_text(encabezado,40,None,(250,70),'center', 'black').draw(self.surface)
         Create_text(text,25,None,(30,120),'left', 'black').draw(self.surface)
 
         self.botones.append({
@@ -113,14 +133,16 @@ class Desicion(Base_win):
             'result': lambda: 'cancelar'
             })
 
-
-
 class Text_return(Base_win):
-    def __init__(self, centro, encabezado, texto) -> None:
+    def __init__(self, centro, encabezado, texto, large=False) -> None:
         super().__init__(centro, encabezado)
         
         Create_text(texto,30,None,(250,70),'center', 'black').draw(self.surface)
-        self.input = Input_text((175,150),(20,150), None)
+        if large:
+            self.input = Input_text((50,150),(20,375), None, border_top_left_radius=20, border_bottom_left_radius=20, max_letter=400)
+        else:
+            self.input = Input_text((175,150),(20,150), None, border_top_left_radius=20, border_bottom_left_radius=20, max_letter=40)
+
         self.input.draw(self.surface)
 
         self.botones.append({
@@ -133,6 +155,11 @@ class Text_return(Base_win):
             'return':'cancelar',
             'result': lambda:'cancelar'
         })
+        self.botones.append({
+            'btn':Create_boton('',25,configs['fuente_simbolos'],(425,150)if large else (325,150), (20,7), 'left','black','white', border_width=1, border_radius=0, border_top_right_radius=20, border_bottom_right_radius=20),
+            'return':'function',
+            'result': lambda: self.input.set(pyperclip.paste())
+        })
         self.inputs.append(self.input)
         
     def draw(self,surface, mouse_pos) -> None:
@@ -141,6 +168,6 @@ class Text_return(Base_win):
     
     def update(self,eventos) -> None:
         for inp in self.inputs:
-            inp.eventos_teclado(eventos,offset=self.rect.topleft)
+            inp.eventos_teclado(eventos)
 
         
