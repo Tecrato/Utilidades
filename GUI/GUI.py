a37dba592b691943fc8802d061a7252b20cfba8f
Tecrato <pinchos792003@gmail.com>
2024-04-02 22:46:11 -0400

    Update GUI.py
    Adaptado el text_return a que las ventanas cambien de tamaño.
    El info tambien.

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -109,10 +109,10 @@ class Base_win:
         return self
 
 class Info(Base_win):
-    def __init__(self,centro: tuple[int,int],encabezado: str,text: str|list[str]):
-        super().__init__(centro,encabezado)
+    def __init__(self,centro: tuple[int,int],encabezado: str,text: str|list[str],size=(500,300)):
+        super().__init__(centro,encabezado,size)
 
-        Create_text(text,25,None,(30,120),'left', 'black').draw(self.surface)
+        Create_text(text,25,None,(30,size[1]/2.3),'left', 'black').draw(self.surface)
 
         self.botones.append({
             'btn':Create_boton('Aceptar',30,None,Vector2(size)-(20,20), 20, 'bottomright','black','white', border_width=-1),
@@ -138,29 +138,29 @@ class Desicion(Base_win):
             })
 
 class Text_return(Base_win):
-    def __init__(self, centro, encabezado, texto, large=False) -> None:
-        super().__init__(centro, encabezado)
+    def __init__(self, centro, encabezado, texto, large=False, size = (500,300)) -> None:
+        super().__init__(centro, encabezado, size)
         
         Create_text(texto,30,None,(250,50),'center', 'black').draw(self.surface)
         if large:
             self.input = Input_text((50,150),(20,375), None, border_top_left_radius=20, border_bottom_left_radius=20, max_letter=400)
         else:
-            self.input = Input_text((175,150),(20,150), None, border_top_left_radius=20, border_bottom_left_radius=20, max_letter=40)
+            self.input = Input_text((size[0]-(150*2.25),150),(20,150), None, border_top_left_radius=20, border_bottom_left_radius=20, max_letter=40)
 
         self.input.draw(self.surface)
 
         self.botones.append({
-            'btn':Create_boton('Aceptar',30,None,(350,280), 20, 'bottomright','black','white', border_width=-1),
-            'return':'aceptar',
-            'result': lambda: self.input.get_text()
-            })
-        self.botones.append({
-            'btn':Create_boton('Cancelar',30,None,(480,280), 20, 'bottomright','black','white', border_width=-1),
+            'btn':Create_boton('Cancelar',30,None,Vector2(size)-(20,20), 20, 'bottomright','black','white', border_width=-1),
             'return':'cancelar',
             'result': lambda:'cancelar'
         })
         self.botones.append({
-            'btn':Create_boton('',25,configs['fuente_simbolos'],(425,150)if large else (325,150), (20,7), 'left','black','white', border_width=1, border_radius=0, border_top_right_radius=20, border_bottom_right_radius=20),
+            'btn':Create_boton('Aceptar',30,None,(Vector2(size)-(20,20))-(self.botones[1]['btn'].rect.w+20,0), 20, 'bottomright','black','white', border_width=-1),
+            'return':'aceptar',
+            'result': lambda: self.input.get_text()
+            })
+        self.botones.append({
+            'btn':Create_boton('',25,configs['fuente_simbolos'],(self.input.rect2.right,150), (20,7), 'left','black','white', border_width=1, border_radius=0, border_top_right_radius=20, border_bottom_right_radius=20),
             'return':'function',
             'result': lambda: self.input.set(pyperclip.paste())
         })
