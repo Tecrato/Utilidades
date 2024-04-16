1da11408720e42915c5e2464396f0a9f727b78cf
Tecrato <pinchos792003@gmail.com>
2024-04-16 16:35:29 -0400

    Update win32_tools.py
    Nueva funcion para saber si hay una ventana llamada [parametro] existe.

--- win32_tools.py
+++ win32_tools.py
@@ -2,7 +2,7 @@ import win32gui, win32con, win32com.client
 def windowEnumerationHandler(hwnd, windows):
     windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-def front(win_name):
+def front(win_name) -> None:
 
     windows = []
     win32gui.EnumWindows(windowEnumerationHandler, windows)
@@ -12,4 +12,13 @@ def front(win_name):
             shell.SendKeys('%')
             win32gui.ShowWindow(i[0],win32con.SW_SHOWNORMAL)# 5
             win32gui.SetForegroundWindow(i[0])
-            break
+            return True
+    return False
+
+def check_win(name) -> bool:
+    windows = []
+    win32gui.EnumWindows(windowEnumerationHandler, windows)
+    for i in windows:
+        if i[1] == name:
+            return True
+    return False
