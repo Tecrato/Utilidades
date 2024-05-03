22f80af26ed0785d08a790c66dbfbe5390a27b72
Tecrato <pinchos792003@gmail.com>
2024-05-02 23:10:09 -0400

    Update web_tools.py

--- web_tools.py
+++ web_tools.py
@@ -12,19 +12,20 @@ def check_update(program_name:str,version_actual:str,version_deseada='last'):
     version_internet = resultado['version'].split('.')
     cuenta_versiones = max(len(version_actual),len(version_internet))
 
-
     for x in range(cuenta_versiones):
         version_actual.append(0)
         version_internet.append(0)
         if int(version_internet[x]) > int(version_actual[x]):
             return {'version': resultado['version'], 'url': resultado['url']}
+        elif int(version_internet[x]) < int(version_actual[x]):
+            return False
     else:
         return False
 
 def get_mediafire_url(url):
-    try:
-        return BeautifulSoup(requests.get(url, allow_redirects=True,timeout=20).content, 'html.parser').find(id='downloadButton').get('href',False)
-    except Exception as err:
-        print(type(err))
-        print(err)
-        return False
+    # try:
+    return BeautifulSoup(requests.get(url, allow_redirects=True,timeout=20).content, 'html.parser').find(id='downloadButton').get('href',False)
+    # except Exception as err:
+    #     print(type(err))
+    #     print(err)
+    #     return False
