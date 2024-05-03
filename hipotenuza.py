a0fe510054809afdb0a4f7c8bcf5f2b4bf39c362
Tecrato <pinchos792003@gmail.com>
2024-05-03 04:57:23 -0400

    Angulo normalized con 0

--- hipotenuza.py
+++ hipotenuza.py
@@ -13,10 +13,9 @@ def Angulo(vector1, vector2) -> float:
     angulo = math.atan2(y,x) * (180.0 / math.pi)
     return angulo if angulo > 0 else 180 + (180+angulo)
 
-def Angulo_normalized(vector1, vector2) -> float:
+def Angulo_normalized(vector1, vector2) -> Vector2:
     x = vector2[0] - vector1[0]
     y = vector2[1] - vector1[1]
-
-    return Vector2(x,y).normalize()
+    return (Vector2(x,y).normalize() if x != 0 or y != 0 else Vector2(0,0))
 
 Angulo_normalized((0,100),(100,125))
