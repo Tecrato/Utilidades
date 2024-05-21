d4392eec7bed141678daa78751a1bb91ff8a26cf
Tecrato <pinchos792003@gmail.com>
2024-05-20 20:40:41 -0400

    Update multithread.py
    Encapsulamiento y nueva funcion para detener todo.

--- multithread.py
+++ multithread.py
@@ -9,30 +9,38 @@ class Funcs_pool:
     def add(self,alias: str, *funcs) -> None:
         if alias in self.threads: raise Exception('Ya existe la key')
         self.threads[f'{alias}'] = {'funcs': funcs}
-        self.running[f'{alias}'] = {}
-        self.running_ids[f'{alias}'] = []
+        if not f'{alias}' in self.running:
+            self.running[f'{alias}'] = {}
+            self.running_ids[f'{alias}'] = []
 
     def join(self,alias, timeout=0) -> None:
         self.running[f'{alias}'][f'{self.running_ids[f'{alias}'][0]}'].join(timeout)
         self.running_ids[f'{alias}'].pop(0)
+
+    def stop_all(self):
+        for x in self.running.keys():
+            for a in range(len(self.running_ids[x])):
+                self.running[f'{x}'][f'{a}'].join(0.01)
+            self.running_ids[f'{x}'].clear()
     
     def stop(self,alias):
         self.running[f'{alias}'][f'{self.running_ids[f'{alias}'][0]}'].join(0.01)
         self.running_ids[f'{alias}'].pop(0)
 
     def start(self,alias) -> None:
-        self.go(alias)
+        self.__go(alias)
 
-    def go(self,alias, id=0) -> None:
-        if f'{id}' in self.running_ids[f'{alias}']: return self.go(alias,id+1)
+    def __go(self, alias, id=0) -> None:
+        if f'{id}' in self.running_ids[f'{alias}']:
+            return self.__go(alias,id+1)
         # self.running[f'{alias}_{id}'] = Thread(target=self.new,args=(alias,id))
         # self.running[f'{alias}_{id}'].start()
         self.running_ids[f'{alias}'].append(id)
-        self.running[f'{alias}'][f'{id}'] = Thread(target=self.new,args=(alias,id), daemon=True)
+        self.running[f'{alias}'][f'{id}'] = Thread(target=self.__new,args=(alias,id), daemon=True)
         self.running[f'{alias}'][f'{id}'].start()
 
-    def new(self,alias, id) -> None:
-        for f in self.threads[f'{alias}']['funcs']:
+    def __new(self,alias, id) -> None:
+        for i,f in enumerate(self.threads[f'{alias}']['funcs']):
             f()
         self.running[f'{alias}'].pop(f'{id}')
         self.running_ids[f'{alias}'].remove(id)
