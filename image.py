a1fd2fcc859bfea8bbfcac7fc09dfff2194afb6b
Tecrato <pinchos792003@gmail.com>
2024-05-20 20:38:11 -0400

    Clase imagen
    Nueva clase de imagen, donde obtiene el path de una imagen y automaticamente resize y convert.
    
    ademas de ser clase hija de Base por lo que tambien obtiene los movimientos y posiciones.

---
+++ image.py
@@ -0,0 +1,31 @@
+import pygame as pag
+from Utilidades.obj_Base import Base
+from PIL import Image as img
+from io import BytesIO
+
+class Image(Base):
+    def __init__(self,image,pos,direccion: str = 'center', size = None,color_key=(254,1,1), dir=[1,0],vel=1):
+        super().__init__(pos,direccion)
+        self.path: str = image
+        if size:
+            im = img.open(self.path)
+            im = im.resize(size)
+            img_bytes = BytesIO()
+            im.save(img_bytes,'PNG')
+            img_bytes.seek(0)
+            self.surf = pag.Surface((im.size))
+            self.image = pag.image.load(img_bytes)
+        else:
+            self.image = pag.image.load(self.path).convert()
+            self.surf = pag.Surface(self.image.get_size())
+        self.surf.fill(color_key)
+        self.surf.set_colorkey(color_key)
+        self.surf.blit(self.image,(0,0))
+        self.rect = self.image.get_rect()
+
+        self.vel = pag.Vector2(dir)*vel
+
+        self.direccion(self.rect)
+
+    def draw(self,surface: pag.Surface):
+        surface.blit(self.surf,self.rect)
