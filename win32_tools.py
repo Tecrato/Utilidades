355d88d7d7e412488c10eee542ff7994ac867a2d
Tecrato <pinchos792003@gmail.com>
2024-05-20 20:43:27 -0400

    Update win32_tools.py
    Nuevo front2, para traer al frente la ventana pasada por el parametro
    
    los front ahora reciben un segundo parametro que define el modo de mostrar la ventana (minimizar, maximizar, mostrar normal).
    
    Nuevas funciones para mover y cambiar de tamaño las ventanas.
    
    Nueva funcion para saber si el equipo tiene el modo oscuro por defecto

--- win32_tools.py
+++ win32_tools.py
@@ -1,19 +1,34 @@
-import win32gui, win32con, win32com.client
+import win32gui, win32con, win32com.client, winreg
+
+
 def windowEnumerationHandler(hwnd, windows):
     windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-def front(win_name) -> None:
-
+def front(win_name,sw_code=1) -> None:
     windows = []
     win32gui.EnumWindows(windowEnumerationHandler, windows)
     for i in windows:
         if i[1] == win_name:
             shell = win32com.client.Dispatch("WScript.Shell")
             shell.SendKeys('%')
-            win32gui.ShowWindow(i[0],win32con.SW_SHOWDEFAULT)# 5
+            win32gui.ShowWindow(i[0],sw_code)# 5
             win32gui.SetForegroundWindow(i[0])
             return True
     return False
+
+def front2(hwnd,sw_code=1):
+    # win32con.SW_SHOW
+    # win32con.SW_SHOWMINIMIZED
+    # win32con.SW_SHOWNORMAL
+    # win32con.SW_MINIMIZE
+    # win32con.SW_MAXIMIZE
+    # win32con.SW_SHOWMAXIMIZED
+    # win32con.SW_MAX
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.SendKeys('%')
+    win32gui.ShowWindow(hwnd,sw_code)# 5
+    win32gui.SetForegroundWindow(hwnd)
+
 
 def check_win(name) -> bool:
     windows = []
@@ -22,3 +37,17 @@ def check_win(name) -> bool:
         if i[1] == name:
             return True
     return False
+
+def moveWin(win,coordinates):
+    hwnd = win
+    win32gui.MoveWindow(hwnd, -coordinates[0], -coordinates[1], 0,0, False)
+def resizeWin(win,coordinates,size):
+    hwnd = win
+    win32gui.MoveWindow(hwnd, -coordinates[0], -coordinates[1], size[0],size[1], False)
+
+
+def is_dark_mode_enabled():
+    registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize")
+    value, _ = winreg.QueryValueEx(registry_key, "AppsUseLightTheme")
+    winreg.CloseKey(registry_key)
+    return value == 0
