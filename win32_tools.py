ccae9ec8fa379d3119b33a24fc703c763e075b34
Tecrato <pinchos792003@gmail.com>
2024-06-20 23:57:52 -0400

    Update win32_tools.py

--- win32_tools.py
+++ win32_tools.py
@@ -1,4 +1,6 @@
-import win32gui, winreg# ,win32con, win32com.client
+import win32gui, winreg
+
+import win32con
 
 def windowEnumerationHandler(hwnd, windows):
     windows.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -8,8 +10,6 @@ def front(win_name,sw_code=1) -> None:
     win32gui.EnumWindows(windowEnumerationHandler, windows)
     for i in windows:
         if i[1] == win_name:
-            # shell = win32com.client.Dispatch("WScript.Shell")
-            # shell.SendKeys('%')
             win32gui.ShowWindow(i[0],sw_code)# 5
             win32gui.BringWindowToTop(i[0])
             win32gui.SetForegroundWindow(i[0])
@@ -50,6 +50,9 @@ def resizeWin(win,coordinates,size):
     hwnd = win
     win32gui.MoveWindow(hwnd, -coordinates[0], -coordinates[1], size[0],size[1], False)
 
+def topmost(win):
+    win32gui.SetWindowPos(win, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+                          win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 
 def is_dark_mode_enabled():
     registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize")
