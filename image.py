1c8b61e62752e4aeb45e4730e7f712085a7e9cca
Tecrato <pinchos792003@gmail.com>
2024-08-18 17:18:40 -0400

    Update image.py
    Ahora ya no va a cargar la imagen denuevo para cambiar de tamaño

--- image.py
+++ image.py
@@ -7,10 +7,10 @@ class Image(Base):
     def __init__(self,image,pos,direccion: str = 'center', size = None,color_key=(254,1,1), dir=[1,0],vel=1):
         super().__init__(pos,direccion)
         self.path: str = image
-        self.__size = tuple(size)
+        self.__size = (int(size[0]),int(size[1]))
         self.color_key = color_key
 
-        
+        self.raw_image = img.open(self.path)
 
         self.cache = {}
         self.generate_img()
@@ -24,22 +24,26 @@ class Image(Base):
         if str(self.__size)+str(self.color_key) in self.cache:
             self.surf = self.cache[str(self.__size) + str(self.color_key)]
             self.rect = self.surf.get_rect()
+            self.direccion(self.rect)
             return
 
         if self.__size:
-            im = img.open(self.path).resize((int(self.__size[0]), int(self.__size[1])))
-            img_bytes = BytesIO()
-            im.save(img_bytes,'PNG')
-            img_bytes.seek(0)
-            self.surf = pag.Surface((im.size))
-            self.image = pag.image.load(img_bytes)
+            im = self.raw_image.resize((int(self.__size[0]), int(self.__size[1])))
         else:
-            self.image = pag.image.load(self.path).convert()
-            self.surf = pag.Surface(self.image.get_size())
+            im = self.raw_image
+
+        img_bytes = BytesIO()
+        im.save(img_bytes,'PNG')
+        img_bytes.seek(0)
+        self.surf = pag.Surface((im.size))
+        self.image = pag.image.load(img_bytes)
+
         self.surf.fill(self.color_key)
         self.surf.set_colorkey(self.color_key)
         self.surf.blit(self.image,(0,0))
         self.rect = self.image.get_rect()
+
+        self.direccion(self.rect)
 
         self.cache[str(self.__size) + str(self.color_key)] = self.surf
 
@@ -50,7 +54,7 @@ class Image(Base):
         return self.__size
     @size.setter
     def size(self,size):
-        self.__size = pag.Vector2(size)
+        self.__size = (int(size[0]),int(size[1]))
         self.generate_img()
 
 
