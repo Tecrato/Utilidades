99cd24f5220560bf6afb2e4cfeba2c68e329d705
Tecrato <pinchos792003@gmail.com>
2024-09-07 00:18:23 -0400

    Update win32_tools.py
    Nuevas funciones:
    las copiaria denuevo pero son muchas y se describen solas.

--- win32_tools.py
+++ win32_tools.py
@@ -1,6 +1,8 @@
-import win32gui, winreg
-
+import win32gui
+import winreg
 import win32con
+import win32ui
+import win32api
 
 def windowEnumerationHandler(hwnd, windows):
     windows.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -74,8 +76,97 @@ def topmost(win):
     win32gui.SetWindowPos(win, win32con.HWND_TOPMOST, 0, 0, 0, 0,
                           win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 
-def is_dark_mode_enabled():
+def get_is_dark_mode_enabled():
     registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize")
     value, _ = winreg.QueryValueEx(registry_key, "AppsUseLightTheme")
     winreg.CloseKey(registry_key)
     return value == 0
+
+def Clip_Cursor(left,top,right,bottom):
+    win32gui.ClipCursor((left,top,right,bottom))
+
+def getFreeDiskSpace(unidad:str='C:/'):
+    r = win32api.GetDiskFreeSpace(unidad)
+    return r[0]*r[1]*r[2]
+
+def getTotalDiskSpace(unidad:str='C:/'):
+    r = win32api.GetDiskFreeSpace(unidad)
+    return r[0]*r[1]*r[3]
+
+def askFile(title='Select a file',filter="Text Files (*.txt)|*.txt|Ejecutables|*.exe|All Files (*.*)|*.*|"):
+    dlg = win32ui.CreateFileDialog(1, None, None, None, filter)
+    # dlg.SetOFNInitialDir('C:/')
+    dlg.SetOFNTitle(title)
+    dlg.DoModal()
+    return dlg.GetPathName()
+
+def askColor(init_color=(0,0,0)):
+    v = win32ui.CreateColorDialog(win32api.RGB(*init_color))
+    v.DoModal()
+    return rgbint2rgbtuple(v.GetColor())
+
+def rgbint2rgbtuple(RGBint):
+    blue =  RGBint & 255
+    green = (RGBint >> 8) & 255
+    red =   (RGBint >> 16) & 255
+    return (red, green, blue)
+
+def MessageBox(title,text,style=[0]):
+    """"
+    
+        0 : win32con.MB_OK,
+        1 : win32con.MB_OKCANCEL,
+        2 : win32con.MB_ABORTRETRYIGNORE,
+        3 : win32con.MB_YESNOCANCEL,
+        4 : win32con.MB_YESNO,
+        5 : win32con.MB_RETRYCANCEL,
+        16 : win32con.MB_ICONHAND,
+        32 : win32con.MB_ICONQUESTION,
+        48 : win32con.MB_ICONEXCLAMATION,
+        64 : win32con.MB_ICONASTERISK,
+        128 : win32con.MB_USERICON,
+
+        12 : win32con.MB_ICONWARNING,
+        13 : win32con.MB_ICONERROR,
+        14 : win32con.MB_ICONINFORMATION,
+        15 : win32con.MB_ICONSTOP,
+
+        0 : win32con.MB_DEFBUTTON1,
+        256 : win32con.MB_DEFBUTTON2,
+        512 : win32con.MB_DEFBUTTON3,
+        768 : win32con.MB_DEFBUTTON4
+
+        262144 : win32con.MB_TOPMOST,
+        524288 : win32con.MB_RIGHT
+    """
+    dict_styles = {
+        0 : win32con.MB_OK,
+        1 : win32con.MB_OKCANCEL,
+        2 : win32con.MB_ABORTRETRYIGNORE,
+        3 : win32con.MB_YESNOCANCEL,
+        4 : win32con.MB_YESNO,
+        5 : win32con.MB_RETRYCANCEL,
+        16 : win32con.MB_ICONHAND,
+        32 : win32con.MB_ICONQUESTION,
+        48 : win32con.MB_ICONEXCLAMATION,
+        64 : win32con.MB_ICONASTERISK,
+        128 : win32con.MB_USERICON,
+
+        12 : win32con.MB_ICONWARNING,
+        13 : win32con.MB_ICONERROR,
+        14 : win32con.MB_ICONINFORMATION,
+        15 : win32con.MB_ICONSTOP,
+
+        0 : win32con.MB_DEFBUTTON1,
+        256 : win32con.MB_DEFBUTTON2,
+        512 : win32con.MB_DEFBUTTON3,
+        768 : win32con.MB_DEFBUTTON4,
+
+        262144 : win32con.MB_TOPMOST,
+        524288 : win32con.MB_RIGHT
+    }
+    s = style[0]
+    for x in style[1:]:
+        s = s | dict_styles[x]
+    
+    return win32ui.MessageBox(text, title, s)
