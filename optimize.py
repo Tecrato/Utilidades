c6fdca15f746406aadbd3437a74570d343fb5fe3
Tecrato <pinchos792003@gmail.com>
2024-10-15 13:05:57 -0400

    Update optimize.py
    Agregado un limite al cache
    
    Para cambiarel limite es:
    Utilidades.optimize.CACHE_LIMIT = n

--- optimize.py
+++ optimize.py
@@ -1,15 +1,16 @@
 from functools import wraps
 
-
+CACHE_LIMIT = 3000
 def memosize(funcion):
     cache = {}
     @wraps(funcion)
     def wrapper(*args,**kwargs):
         key = str(args) + str(kwargs)
-
+        l = len(cache)
+        if l >= CACHE_LIMIT:
+            cache.clear()
         if key not in cache:
             cache[key] = funcion(*args, **kwargs)
-
         return cache[key]
     return wrapper
 
